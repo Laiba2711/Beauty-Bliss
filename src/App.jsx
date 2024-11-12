d7e5dd30d3a8b7684d918229b1fcbcedea3f3c9a
Laiba Rashid <laibarashid2711@gmail.com>
2024-11-13 02:34:03 +0500

    herosection

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HeroSection from './Pages/HeroSection'
+
+export default function AppComp() {
+  return (
+    <HeroSection />
+  )
+}
---
+++ src/Pages/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../style.css"
+export default function HeroSection() {
+  return (
+    <>
+   <div className="heroSec">
+    <div className="container">
+      <div className="image-section left">
+        <img src="./BB-HERO2.jpg" alt="Left Image" />
+      </div>
+      <h1 className="title">𝓑𝓮𝓪𝓾𝓽𝔂𝓑𝓵𝓲𝓼𝓼</h1>
+      <h2 className="title2">Enjoy shopping with Dior.</h2>
+      <div className="image-section right">
+        <img src="./BB-HERO3.jpg" alt="Right Image"/>
+      </div>
+    </div>
+  </div>
+    </>
+    
+  )
+}
