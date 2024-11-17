2fbc1c4ee90a9a50c86ccbdeccccec70945dc45f
Laiba Rashid <laibarashid2711@gmail.com>
2024-11-18 00:40:31 +0500

    Revert "updated files"

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import HeroSection from './Pages/HeroSection'
 import Cards from './Pages/CardSection'
 import CardNav from './Pages/CardNav'
-import Navbar from './Components/Navbar'
 
 export default function AppComp() {
   return (
     <>
-    <Navbar/>
     <HeroSection />
     <CardNav />
     </>
