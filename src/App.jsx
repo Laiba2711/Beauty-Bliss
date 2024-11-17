eb094ad11eed36e0d787ef860aef4814f6702cac
Kaneez Fatima <kfatima16502@gmail.com>
2024-11-17 10:03:59 +0500

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import HeroSection from './Pages/HeroSection'
 import Cards from './Pages/CardSection'
 import CardNav from './Pages/CardNav'
+import Navbar from './Components/Navbar'
 
 export default function AppComp() {
   return (
     <>
+    <Navbar/>
     <HeroSection />
     <CardNav />
     </>
