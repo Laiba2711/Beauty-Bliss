40587a5a68861452ef541570e352f7e4ce0e4c6c
Kaneez Fatima <kfatima16502@gmail.com>
2024-11-18 02:29:35 +0500

    Resolve merge conflict by incorporating both suggestions

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,42 +1,20 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-
 export default function() {
   return (
-   <nav className='navbar'>
-   <div className='header'>
-   <img src="logo1.JPG" alt=""  height="120px"/>
-    <ul className='header-menu'>
-    <a class="active" href="#home">
-    <i class="fa-solid fa-house-user" aria-hidden="true"></i> HOME</a>
-  <a href="#about">
-  <i class="fa-solid fa-circle-info" aria-hidden="true"></i>  ABOUT</a>
-  <a href="#contact">
-  <i class="fa-solid fa-briefcase" aria-hidden="true"></i>   SERVICES</a>
-  <a href="#contact">
-  <i class="fa-solid fa-address-book" aria-hidden="true"></i>   CONTACT</a>
-  <a href="#contact">
-  <i class="fa-solid fa-user" aria-hidden="true"></i>  PROFILE</a>
-  
+    <nav>
+    <input type="checkbox" id="check"/>
+    <label for="check" class="checkbtn">
+      <i class="fas fa-bars"></i>
+    </label>
+   <img src="logo1.JPG" alt="" height="100px"   />
+    <ul className='nav-icons'>
+      <li><a class="active" href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Feedback</a></li>
     </ul>
-   
-   </div>
-   <div className='topnav'>
-   <div className="menu">
-  <a class="active" href="#home">ALL</a>
-  <a href="#about">FACES</a>
-  <a href="#contact">EYES</a>
-  <a href="#contact">LIPS</a>
-  </div>
-  <div class="search-container">
-            <input  type="text" id="searchBar" placeholder="Type to search..."  onchange="searchData()"  />
-            <button type="submit"><i class="fa fa-search"></i></button>
-        </div>
-        <div className="cart">
-        <i class="fa fa-shopping-cart" ></i>
-        </div>
-   </div>
-   
-   </nav>
+  </nav>
   )
 }
