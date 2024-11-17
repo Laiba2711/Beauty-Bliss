a9693adcf3b08e1be91de068a199dc2ed6a7408c
Kaneez Fatima <kfatima16502@gmail.com>
2024-11-18 01:41:21 +0500

    again check

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import HeroSection from './Pages/HeroSection';
-import Cards from './Pages/CardSection';
+// import Cards from './Pages/CardSection';
 import CardNav from './Pages/CardNav';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 export default function AppComp() {
   return (
     <>
     <Navbar/>
     <HeroSection />
     <CardNav />
+    <Footer />
     </>
   )
 }
